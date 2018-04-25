0961dc90a111fb57b8c65131bb461185d44dcdb2
HxA <henock.a@hotmail.de>
2018-04-25 20:57:25 +0200

    Illumination still not working...

--- gencg/hareg001/objects.py
+++ gencg/hareg001/objects.py
@@ -7,20 +7,21 @@ import time
 
 
 class Color(object):
+	_VALDICT = {"r": 0, "g": 1, "b": 2}
+	_REPR = "Color(%d,%d,%d)"
 
 	def __init__(self, r, g, b):
 		self.values = (r, g, b)
-		self._REPR = "Color(%d,%d,%d)"
-		self._VALDICT = {"r":0, "g":1, "b":2}
 
 	def __repr__(self):
 		return self._REPR % tuple(self.values)
 
-	def toRGB(self): return tuple(self.values)
+	def toRGB(self):
+		return tuple(self.values)
 
 	def __add__(self, other):
 		if type(other) == Color:
-			return eval( self._REPR	% tuple(map(lambda x, y: x+y if x+y <= 255 else 255, self.values, other)) )
+			return eval(self._REPR % tuple(map(lambda x, y: x + y if x + y <= 255 else 255, self.values, other)))
 
 	def __getitem__(self, item):
 		if type(item) == int:
@@ -29,9 +30,53 @@ class Color(object):
 			return self.values[self._VALDICT[item]]
 
 	def __mul__(self, other):
-		return eval(self._REPR  % tuple(map(lambda x: x * other, self.values)))
+		return eval(self._REPR % tuple(map(lambda x: x * other, self.values)))
 
-	def __truediv__(self, other): return eval(self.__mul__(1/other))
+	def __truediv__(self, other):
+		return eval(self.__mul__(1 / other))
+
+
+class Material(object):
+
+	def __init__(self, ambient, ambientLvl, diffuse, diffuseLvl, spec, specLvl):
+		self.ambient = ambient  # color
+		self.ambientLvl = ambientLvl  # coefficient
+		self.diffuse = diffuse  # color
+		self.diffuseLvl = diffuseLvl  # coefficient
+		self.spec = spec  # color
+		self.specLvl = specLvl  # coefficient
+
+	def color(self, diffMulti=0, specMulti=0):
+		diffMulti = cos(diffMulti)
+		specMulti = cos(specMulti)
+
+		diffuse = self.diffuse * self.diffuseLvl * diffMulti
+		spec = self.spec * self.specLvl * specMulti
+
+		components = [diffuse, spec]
+
+		for c in components:
+			for val in c.toRGB():
+				if val < 0:
+					c = black
+					break
+				elif val > 255:
+					c = 255
+					break
+
+		_ambient = self.ambient * self.ambientLvl
+		_diffuse = diffuse * diffMulti * self.diffuseLvl
+		_specular = spec * specMulti * self.specLvl
+
+		return _ambient + _diffuse + _specular
+
+	def __repr__(self):
+		return "Material({},{},{},{},{},{},{})".format(
+				repr(self.color),
+				self.ambient, self.ambientLvl,
+				self.diffuse, self.diffuseLvl,
+				self.spec, self.specLvl)
+
 
 white = Color(255, 255, 255)
 grey = Color(128, 128, 128)
@@ -41,16 +86,16 @@ black = Color(0, 0, 0)
 red = Color(255, 0, 0)
 green = Color(0, 255, 0)
 blue = Color(0, 0, 255)
-yellow = Color(255,255,0)
+yellow = Color(255, 255, 0)
+
 
 class Vector(object):
-
 	# TODO: flexible amount of coordinates; in list
+	_REPR = "Vector(%f, %f, %f)"
 
 	def __init__(self, x, y, z):
 		# self.x, self.y, self.z, self.point = float(x), float(y), float(z), float(point)
 		self.coordinates = [x, y, z]
-		self._REPR = "Vector(%f, %f, %f)"
 
 	def scalar(self, vector):
 		return sum(list(map(lambda x, y: x * y, self, vector)))
@@ -81,12 +126,13 @@ class Vector(object):
 		return tuple(self.coordinates)
 
 	def length(self):
+		"""Returns the length of a vector."""
 		return sqrt(sum(list(map(lambda x: x ** 2, self.coordinates))))
 
 	def normalize(self):
 		length = self.length()
-		return eval( self._REPR %
-				tuple( map(lambda x: x/length, self.coordinates) ) )
+		return eval(self._REPR %
+					tuple(map(lambda x: x / length, self.coordinates)))
 
 	def scale(self, t):
 		return eval(self._REPR % tuple(map(lambda x: x * t, self.coordinates)))
@@ -109,14 +155,14 @@ class Vector(object):
 	def __len__(self):
 		return len(self.coordinates)
 
-	def __pow__(self, power, modulo=None):
-		return  eval( self._REPR % tuple(map(lambda x: x**power, self.coordinates)) )
+	def __pow__(self, exp, modulo=None):
+		return eval(self._REPR % tuple(map(lambda x: x ** exp, self.coordinates)))
 
 	def __repr__(self):
 		return self._REPR % self.tuple()
 
 	def __truediv__(self, t):
-		return self.scale(1/t)
+		return self.scale(1 / t)
 
 
 class GraphicsObject(object):
@@ -183,20 +229,22 @@ class Sphere(GraphicsObject):
 		self.rad, self.center = rad, center  # rad:=number; center:=point
 		self.mat = mat
 
-	def __repr__(self): return "Sphere({},{},{})".format(self.rad, repr(self.center), repr(self.mat))
+	def __repr__(self):
+		return "Sphere({},{},{})".format(self.rad, repr(self.center), repr(self.mat))
 
 	def intersection(self, ray):
 		co = self.center - ray.origin  # co = c-o
 		v = co.scalar(ray.direction)
 
-		discriminant = v**2 - co.scalar(co) + self.rad ** 2
+		discriminant = v ** 2 - co.scalar(co) + self.rad ** 2
 
 		if discriminant < 0:
 			return None
-		else: # nur wenn der punkt vor der camera ist; nicht hinter
+		else:  # nur wenn der punkt vor der camera ist; nicht hinter
 			return v - sqrt(discriminant)
 
-	def normalAt(self, point): return (point - self.center).normalize()
+	def normalAt(self, point):
+		return (point - self.center).normalize()
 
 
 # –––––––––––––––– - –––––––––––––––– #
@@ -215,68 +263,26 @@ class Ray(object):
 class Light(object):
 
 	def __init__(self, origin, intensity, color):
-		self.origin, self.intensity = origin, intensity #origin = vector
+		self.origin, self.intensity = origin, intensity  # origin = vector
 		self.color = color
-
-
-class Material(object):
-
-	def __init__(self, ambient, ambientLvl, diffuse, diffuseLvl, spec, specLvl):
-		self.ambient = ambient # color
-		self.ambientLvl = ambientLvl # coefficient
-		self.diffuse = diffuse # color
-		self.diffuseLvl = diffuseLvl # coefficient
-		self.spec = spec # color
-		self.specLvl = specLvl # coefficient
-
-	def color(self, diffMulti=0, specMulti=0):
-		diffMulti = cos(diffMulti)
-		specMulti = cos(specMulti)
-
-		diffuse = self.diffuse * self.diffuseLvl * diffMulti
-		spec = self.spec * self.specLvl * specMulti
-
-		components = [diffuse, spec]
-
-		for c in components:
-			for val in c.toRGB():
-				if val < 0:
-					c = black
-					break
-				elif val > 255:
-					c = 255
-					break
-
-		_ambient = self.ambient * self.ambientLvl
-		_diffuse = diffuse * diffMulti * self.diffuseLvl
-		_specular = spec * specMulti * self.specLvl
-
-		return _ambient + diffuse + _specular
-
-	def __repr__(self):
-		return "Material({},{},{},{},{},{},{})".format(
-				repr(self.color),
-				self.ambient, self.ambientLvl,
-				self.diffuse, self.diffuseLvl,
-				self.spec, self.specLvl)
 
 
 class Camera(object):
 
 	def __init__(self, origin, focus, up, fieldOfView):
 		self.origin = origin  # points
-		self.focus = focus # the point to which the camera is aiming at
-		self.up = up # global up vector/direction of up
-		self.fov = fieldOfView # field of view
+		self.focus = focus  # the point to which the camera is aiming at
+		self.up = up  # global up vector/direction of up
+		self.fov = fieldOfView  # field of view
 		self.alpha = self.fov / 2
 
 		ce = self.focus - self.origin
-		self.f = ce / ce.length() # perpendicular u vector (x-axis)
+		self.f = ce / ce.length()  # perpendicular u vector (x-axis)
 
 		fxup = self.f.cross(self.up)
-		self.s = fxup / fxup.length() # s vector (z-axis)
+		self.s = fxup / fxup.length()  # s vector (z-axis)
 
-		self.u = self.s.cross(self.f) # f vector (y-axis)
+		self.u = self.s.cross(self.f)  # f vector (y-axis)
 
 	def __repr__(self):
 		return "Camera({},{},{},{},{},{})".format(
@@ -289,7 +295,7 @@ class Camera(object):
 
 class Picture(object):
 
-	def __init__(self, resX, resY, camera, light, *objects):
+	def __init__(self, resX, resY, camera, light, objects):
 		self.resX, self.resY = resX, resY
 		self.ratio = float(resX) / float(resY)
 
@@ -301,10 +307,11 @@ class Picture(object):
 		self.height = 2 * tan(self.camera.alpha)
 		self.width = self.ratio * self.height
 
-		self.pixelW, self.pixelH = self.width / (self.resX-1), self.height / (self.resY-1)
+		self.pixelW, self.pixelH = self.width / (self.resX - 1), self.height / (self.resY - 1)
 
 	def castRays(self):
-		_prop_str = "_".join([str("W" + str(self.resX)), str("H" + str(self.resY)), str("FOV" + str(self.camera.fov))])
+		# _prop_str = "_".join([str("W" + str(self.resX)), str("H" + str(self.resY)), str("FOV" + str(self.camera.fov))])
+		_prop_str = ""
 		self.image = Image.new("RGB", (self.resX, self.resY))
 
 		total = colorTotal = 0
@@ -312,29 +319,36 @@ class Picture(object):
 		for x in range(self.resX):
 			for y in range(self.resY):
 				# for each pixel of the image ...
-				maxDistance = float('inf')
-				total += 1
-				ray = self.calcRay(x, y) # .. cast a ray
 				color = black
+				maxDistance = float('inf')
 
+				total += 1
+
+				ray = self.calcRay(x, y)  # .. cast a ray
+				hitdist = None
+				obj_ = None
 				for obj in self.objects:
+					obj_ = obj
 					hitdist = obj.intersection(ray)
-					if hitdist is not None:
+					if hitdist:
 						# if the ray intersected an object ...
 
 						if hitdist < maxDistance:
 							# continue until its closest point has been found
 							# and then color the pixel
 							maxDistance = hitdist
-							color = self.calcIllumination(obj, ray, hitdist)
-
+							#color = self.calcIllumination(obj, ray, hitdist)
+				if hitdist:
+					color = self.calcIllumination(obj_, ray, hitdist)
 				self.image.putpixel((x, y), color.toRGB())
 
 				if color != black:
 					colorTotal += 1
-					#print("%.2f%%" % (colorTotal / total * 100))
+			# print("%.2f%%" % (colorTotal / total * 100))
 
-		self.image.save("/Users/HxA/PycharmProjects/RayTracer" + str(int(round(time.time() * 1000))) + "_" + _prop_str + ".jpg","JPEG",quality=75)
+		self.image.save(
+			"/Users/HxA/PycharmProjects/RayTracer" + str(int(round(time.time() * 1000))) + "_" + _prop_str + ".jpg",
+			"JPEG", quality=75)
 		self.image.show()
 
 	def calcRay(self, x, y):
@@ -343,16 +357,15 @@ class Picture(object):
 		return Ray(self.camera.origin, self.camera.f + xComp + yComp)
 
 	def calcIllumination(self, object, ray, dist):
-		#angel between two vectors: <v,w> / ||v||*||w||
+		# angel between two vectors: <v,w> / ||v||*||w||
 		light_vect = self.light.origin
 
-		intersectionP = ray.direction * dist # vector towards the object/intersection point #vector
+		intersectionP = ray.direction * ((-1)*dist)  # vector towards the object/intersection point #vector
 		l = light_vect - intersectionP
 		n = object.normalAt(intersectionP)
 		d = ray.origin - intersectionP
 
 		_diff_scalar = light_vect.normalize().scalar(object.normalAt(intersectionP))
-
 
 		phi = l.normalize().scalar(n) / (l.length() * n.length())
 
--- main.py
+++ main.py
@@ -4,50 +4,19 @@ WIDTH = 400
 HEIGHT = 300
 
 if __name__ == "__main__":
-
-    print(black)
-
     up = Vector(0, -1, 0)
     focus_point = Vector(0, 0, 250)
 
+    sphere = Sphere(30, Vector(0, 0, 100),
+                    mat=Material(red, 0.1, green, 0.3, blue, 0.4))
     plane = Plane(
             Vector(0, -60, 0),
             Vector(0, 1, 0),
-            Material(white, 1, red, 1.6, white, 1)
+            mat=Material(red, 0.4, green, 0.71, blue, 0.3)
     )
-
-    mat1 = Material(red, 1, red*1.3, 1, red, 1)
-    sphere = Sphere(30, Vector(0, 0, 100), mat1)
 
     camera = Camera(Vector(0,0,-50),focus_point, up, 120)
     light = Light(Vector(100, 200, -75), 1, white)
 
-    img = Picture(WIDTH, HEIGHT, camera, light, sphere, plane)
+    img = Picture(WIDTH, HEIGHT, camera, light, [sphere, plane])
     img.castRays()
-
-else :
-    v0 = Vector(2, 3, 4)
-    v1 = Vector(3, 6, 2)
-
-    v0 = Vector(1, 2, 3)
-    v1 = Vector(2, 1, 3)
-
-    v0 = Vector(1, -2, 1)
-    v1 = Vector(4, 3, 0)
-
-    print(v0, v1)
-    print(v0.length(), v1.length())
-    print(v0.tuple(), v1.tuple())
-    print(v0.cross(v1))
-    print(v0.scalar(v1))
-
-    print("Hess'che", v0.normalize())
-
-    print("ADD:", (v0 + v1))
-
-    t = Triangle(3, 2, 4)
-
-    c = Camera(v0, 400, 400, 60, v1, Vector(300, 200, 132))
-    print(c)
-
-    print(t)
